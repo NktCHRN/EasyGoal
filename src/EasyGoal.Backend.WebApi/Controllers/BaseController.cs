4d949c3f84dbbea20cae3b197aaf7fe9c5050f28
Chernikov Nikita <14nik20@gmail.com>
2024-06-02 22:48:31 +0300

    Added BaseController.PagedOkResponse

--- src/EasyGoal.Backend.WebApi/Controllers/BaseController.cs
+++ src/EasyGoal.Backend.WebApi/Controllers/BaseController.cs
@@ -4,6 +4,11 @@ using Microsoft.AspNetCore.Mvc;
 namespace EasyGoal.Backend.WebApi.Controllers;
 public abstract class BaseController : ControllerBase
 {
+    public OkObjectResult PagedOkResponse<T>(IEnumerable<T> items, PaginationParametersResponse paginationParameters)
+    {
+        return base.Ok(PagedApiResponse.FromResult(items, paginationParameters));
+    }
+
     public OkObjectResult OkResponse<T>(T value)
     {
         return base.Ok(ApiResponse.FromResult(value));
