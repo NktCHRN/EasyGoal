368b4bf2488a88d3712ad6ddd3d55cfe135eb25d
Chernikov Nikita <14nik20@gmail.com>
2024-06-02 22:49:12 +0300

    Changed access modifiers to protected

--- src/EasyGoal.Backend.WebApi/Controllers/BaseController.cs
+++ src/EasyGoal.Backend.WebApi/Controllers/BaseController.cs
@@ -4,27 +4,27 @@ using Microsoft.AspNetCore.Mvc;
 namespace EasyGoal.Backend.WebApi.Controllers;
 public abstract class BaseController : ControllerBase
 {
-    public OkObjectResult PagedOkResponse<T>(IEnumerable<T> items, PaginationParametersResponse paginationParameters)
+    protected OkObjectResult PagedOkResponse<T>(IEnumerable<T> items, PaginationParametersResponse paginationParameters)
     {
         return base.Ok(PagedApiResponse.FromResult(items, paginationParameters));
     }
 
-    public OkObjectResult OkResponse<T>(T value)
+    protected OkObjectResult OkResponse<T>(T value)
     {
         return base.Ok(ApiResponse.FromResult(value));
     }
 
-    public OkObjectResult OkResponse()
+    protected OkObjectResult OkResponse()
     {
         return base.Ok(ApiResponse.FromResult(new object()));
     }
 
-    public NoContentResult NoContentResponse()
+    protected NoContentResult NoContentResponse()
     {
         return base.NoContent();
     }
 
-    public UnauthorizedObjectResult UnauthorizedResponse(ErrorResponse error)
+    protected UnauthorizedObjectResult UnauthorizedResponse(ErrorResponse error)
     {
         return base.Unauthorized(ApiResponse.FromError(error));
     }
