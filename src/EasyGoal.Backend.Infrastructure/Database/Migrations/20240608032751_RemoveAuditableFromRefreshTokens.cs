20594cd3eb79780c4b1ad5035124f040c680154f
Chernikov Nikita <14nik20@gmail.com>
2024-06-08 06:28:14 +0300

    Removed Auditable fields from RefreshTokens

---
+++ src/EasyGoal.Backend.Infrastructure/Database/Migrations/20240608032751_RemoveAuditableFromRefreshTokens.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EasyGoal.Backend.Infrastructure.Database.Migrations;
+
+/// <inheritdoc />
+public partial class RemoveAuditableFromRefreshTokens : Migration
+{
+    /// <inheritdoc />
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.DropColumn(
+            name: "CreatedAt",
+            table: "RefreshTokens");
+
+        migrationBuilder.DropColumn(
+            name: "CreatedBy",
+            table: "RefreshTokens");
+
+        migrationBuilder.DropColumn(
+            name: "ModifiedAt",
+            table: "RefreshTokens");
+
+        migrationBuilder.DropColumn(
+            name: "ModifiedBy",
+            table: "RefreshTokens");
+    }
+
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.AddColumn<DateTimeOffset>(
+            name: "CreatedAt",
+            table: "RefreshTokens",
+            type: "timestamp with time zone",
+            nullable: false,
+            defaultValue: new DateTimeOffset(new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)));
+
+        migrationBuilder.AddColumn<string>(
+            name: "CreatedBy",
+            table: "RefreshTokens",
+            type: "character varying(256)",
+            maxLength: 256,
+            nullable: true);
+
+        migrationBuilder.AddColumn<DateTimeOffset>(
+            name: "ModifiedAt",
+            table: "RefreshTokens",
+            type: "timestamp with time zone",
+            nullable: true);
+
+        migrationBuilder.AddColumn<string>(
+            name: "ModifiedBy",
+            table: "RefreshTokens",
+            type: "character varying(256)",
+            maxLength: 256,
+            nullable: true);
+    }
+}
--- src/EasyGoal.Backend.Infrastructure/Identity/RefreshToken.cs
+++ src/EasyGoal.Backend.Infrastructure/Identity/RefreshToken.cs
@@ -1,11 +1,16 @@
 ﻿using EasyGoal.Backend.Domain.Abstractions.Entities;
+using MassTransit;
 
 namespace EasyGoal.Backend.Infrastructure.Identity;
-public class RefreshToken : BaseAuditableEntity
+public class RefreshToken : ISoftDeletableEntity
 {
+    public Guid Id { get; set; } = NewId.NextSequentialGuid();
+
     public string Token { get; set; } = string.Empty;
 
     public DateTimeOffset ExpiryTime { get; set; }
+
+    public bool IsDeleted { get; set; }
 
     public Guid UserId { get; set; }
     public IdentityApplicationUser User { get; set; } = null!;
